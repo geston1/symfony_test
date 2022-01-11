ccfe7ac619e47a525c7c6ce0dbaa5be79ec89a76
nosferatu <u.isaichkin@fim.ltd>
2022-01-11 22:15:57 +0300

    backend.jwt_auth

---
+++ backend/src/Controller/Api/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/api/user", name="api_user")
+     */
+    public function index(UserInterface $user): Response
+    {
+        return new JsonResponse([
+            'data' => $user->getUsername()
+        ]);
+    }
+}
